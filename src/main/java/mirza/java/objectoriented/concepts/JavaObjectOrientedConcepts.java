07376fb64dc09d82b2e4b35f2a52c0d003ce8577
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2024-08-20 00:27:33 +0600

    object_and_class_example_done

--- src/main/java/mirza/java/objectoriented/concepts/JavaObjectOrientedConcepts.java
+++ src/main/java/mirza/java/objectoriented/concepts/JavaObjectOrientedConcepts.java
@@ -2,6 +2,17 @@ package mirza.java.objectoriented.concepts;
 
 public class JavaObjectOrientedConcepts {
     public static void main(String[] args){
-        System.out.println("1");
+        // creating an object and instance of ObjectAndClass
+        ObjectAndClass objectAndClass = new ObjectAndClass();
+
+        // accessing member through reference variable
+        objectAndClass.studentName = "Mirza Mahmud Hossan";
+        objectAndClass.studentId = 17182103202L;
+        objectAndClass.studentAge = 25;
+
+        //Printing values of the object
+        System.out.println("Student Name: " + objectAndClass.studentName);
+        System.out.println("Student ID: " + objectAndClass.studentId);
+        System.out.println("Student Age: " + objectAndClass.studentAge);
     }
 }
---
+++ src/main/java/mirza/java/objectoriented/concepts/ObjectAndClass.java
@@ -0,0 +1,10 @@
+package mirza.java.objectoriented.concepts;
+
+// defining a class which named is ObjectAndClass
+public class ObjectAndClass {
+
+    // defining fields or data members or instance variable
+    String studentName;
+    long studentId;
+    int studentAge;
+}
